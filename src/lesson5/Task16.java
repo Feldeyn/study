c29baf4e2e7f25d0ffe7f925d763e5a901ba3210
Happyness <Dron120383@gmail.com>
2018-07-06 15:19:16 +0300

    ДЗ третьего урока. Часть четвертого и пятого

---
+++ src/lesson5/Task16.java
@@ -0,0 +1,26 @@
+package lesson5;
+
+import java.util.Arrays;
+
+public class Task16 {
+    public static void main(String[] args) {
+        int k=0;
+        int[] pusurok={34,12,56,23,7};
+        for (int i=0; i<pusurok.length-1;i++){
+            for(int j=0; j<pusurok.length-(1+i); j++){
+                if (pusurok[j+1]>pusurok[j]){
+                    int tmp=pusurok[j];
+                    pusurok[j]=pusurok[j+1];
+                    pusurok[j+1]=tmp;
+                    System.out.println(Arrays.toString(pusurok));
+                    k=0;
+                } else {
+                    k=k+1;
+                    if (k==pusurok.length-i){
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
