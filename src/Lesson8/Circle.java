35c1b7a56747e8967825ce8c5ae938b9dd0e0f92
GreatUrsa <thegreat.bear@mail.ru>
2018-07-09 21:32:45 +0300

    Урок 8

--- src/Lesson8/Circle.java
+++ src/Lesson8/Circle.java
@@ -3,16 +3,16 @@ package Lesson8;
 public class Circle extends Shape {
     private int rad;
 
+    public Circle(double x, double y, String color, int rad) {
+        super(x, y, color);
+        this.rad = rad;
+    }
+
     public int getRad() {
         return rad;
     }
 
     public void setRad(int rad) {
-        this.rad = rad;
-    }
-
-    public Circle(double x, double y, String color, int rad) {
-        super(x, y, color);
         this.rad = rad;
     }
 
--- src/Lesson8/Rectandle.java
+++ src/Lesson8/Rectandle.java
@@ -3,16 +3,16 @@ package Lesson8;
 public class Rectandle extends Shape {
     private double z;
 
+    public Rectandle(double x, double y, String color, double z) {
+        super(x, y, color);
+        this.z = z;
+    }
+
     public double getZ() {
         return z;
     }
 
     public void setZ(int z) {
-        this.z = z;
-    }
-
-    public Rectandle(double x, double y, String color, double z) {
-        super(x, y, color);
         this.z = z;
     }
 
